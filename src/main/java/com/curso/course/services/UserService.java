af2ca2703bac9ecfebf02f4e32d8a19b8b53c7f3
Rodrigo Andrade <rodrigom.andrade95@gmail.com>
2020-06-02 11:47:22 -0300

    Exception handling

--- src/main/java/com/curso/course/services/UserService.java
+++ src/main/java/com/curso/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.curso.course.entities.User;
 import com.curso.course.repositories.UserRepository;
+import com.curso.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
